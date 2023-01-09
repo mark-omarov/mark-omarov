564ebfc160b3ddf9d439565dbf5db897eaea6f38
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2023-01-09 18:13:38 +0900

    feat: add favicon (#13)
    * feat: add favicon
    
    * fix: collapsed avatar on image load

--- apps/personal-website/src/components/Avatar/Avatar.tsx
+++ apps/personal-website/src/components/Avatar/Avatar.tsx
@@ -1,8 +1,8 @@
 import { component$, Slot } from "@builder.io/qwik";
 
 export const Avatar = component$(() => (
-  <div class="relative w-40">
-    <div class="rounded-full overflow-clip bd border border-border">
+  <div class="relative w-40 h-40">
+    <div class="w-full h-full rounded-full overflow-clip bd border border-border">
       <img class="pointer-events-none" src="/images/avatar.jpeg" alt="Avatar" />
     </div>
     <Slot />
