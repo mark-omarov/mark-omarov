f36ee9533c7b3dff05ad54f24be7bdeaf7dff9ce
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2025-05-03 10:35:19 +0900

    feat: scaffold website

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
